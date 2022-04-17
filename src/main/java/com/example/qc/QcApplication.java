f8e92567a4a26c1d1ca4cf5e3c2112374fa1a579
李毅\liyi <1317208044@qq.com>
2022-04-17 12:13:20 +0800

    Fix

---
+++ src/main/java/com/example/qc/QcApplication.java
@@ -0,0 +1,13 @@
+package com.example.qc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qc/QcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
